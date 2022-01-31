8d42beffad4d864ea0fdb4ab8c2028d600be20f0
achiyahaviv <ahiya@coing.co>
2022-01-31 20:23:12 +0200

    add Google Analytics integration

--- components/OgMetaTags.js
+++ components/OgMetaTags.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import React from 'react'
+import crossEnv from "cross-env";
 
-export default function OgMetaTags({url,title,description,img}) {
+export default function OgMetaTags({url, title, description, img}) {ֿ
+    const googleAnalyticsTrackingId = crossEnv(process.env.GOOGLE_ANALYTICS_TRACKING_ID)
     return (
         <div>
             <Head>
@@ -9,7 +11,7 @@ export default function OgMetaTags({url,title,description,img}) {
                     {title}
                 </title>
                 <meta name="twitter:image" content={img}/>
-                <meta property="og:type" content="website" />
+                <meta property="og:type" content="website"/>
                 <meta property="og:url" content={url}/>
                 <meta
                     property="og:title"
@@ -19,7 +21,22 @@ export default function OgMetaTags({url,title,description,img}) {
                     property="og:description"
                     content={description}
                 />
-                <meta property="og:image"  content={img}/>
+                <meta property="og:image" content={img}/>
+                <script
+                    async
+                    src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsTrackingId}`}
+                />
+
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${googleAnalyticsTrackingId}', { page_path: window.location.pathname });
+            `,
+                    }}
+                />
             </Head>
         </div>
     );
