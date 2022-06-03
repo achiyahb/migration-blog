84b5234559544e20bb539c83d529dfc2dedd695d
achiyahb <achiyahb@gmail.com>
2022-06-03 12:07:28 +0300

    use env for profiles urls

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,21 @@
 import styles from '../styles/Header.module.css'
-import {FaFacebook, FaGithub, FaLinkedinIn,FaAngleRight} from "react-icons/fa";
+import {FaFacebook, FaGithub, FaLinkedinIn, FaAngleRight} from "react-icons/fa";
 import Link from "next/link";
-import React,{useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {useRouter} from "next/router";
 
 
 export default function Header() {
     const router = useRouter()
 
-    const [backArrow, setBackArrow]= useState(true)
+    const [backArrow, setBackArrow] = useState(true)
     useEffect(() => {
         setBackArrow(router.asPath !== '/')
     }, [router.asPath]);
 
-    const goToLinkHandler = (name)=>{
-        window.open(name === 'facebook' ? 'https://www.facebook.com/profile.php?id=100013305678737' :
-            name === 'github' ? 'https://github.com/achiyahb' : 'https://www.linkedin.com/in/achiya-haviv/', '_blank')
+    const goToLinkHandler = (name) => {
+        window.open(name === 'facebook' ? process.env.FACEBOOK_PROFILE_URL :
+            name === 'github' ? process.env.GITHUB_PROFILE_URL : process.env.LINKEDIN_PROFILE_URL, '_blank')
     }
 
     return (
@@ -24,7 +24,7 @@ export default function Header() {
                 <Link
                     href={`/`}
                 ><a>
-                <FaAngleRight  className={backArrow ? styles.home_icon : styles.none}  size={35}/>
+                    <FaAngleRight className={backArrow ? styles.home_icon : styles.none} size={35}/>
                 </a>
                 </Link>
             </div>
@@ -40,11 +40,11 @@ export default function Header() {
             </div>
 
             <div className={styles.icons}>
-                <FaGithub className={styles.icon} size={35} onClick={()=>goToLinkHandler('github')}/>
-                <div className={styles.linked_icon} onClick={()=>goToLinkHandler('linkedin')}>
-                    <FaLinkedinIn  size={25} />
+                <FaGithub className={styles.icon} size={35} onClick={() => goToLinkHandler('github')}/>
+                <div className={styles.linked_icon} onClick={() => goToLinkHandler('linkedin')}>
+                    <FaLinkedinIn size={25}/>
                 </div>
-                <FaFacebook className={styles.icon}  size={35} onClick={()=>goToLinkHandler('facebook')}/>
+                <FaFacebook className={styles.icon} size={35} onClick={() => goToLinkHandler('facebook')}/>
             </div>
         </div>
     )
