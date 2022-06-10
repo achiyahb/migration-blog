de340170bacb3e964424ee8829e41a4d48af4b30
achiyahb <achiyahb@gmail.com>
2022-06-10 13:51:24 +0300

    change from then api to async await inside posts api

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,4 @@
 import styles from '../styles/Layout.module.css'
-import Head from "next/head";
 import Header from "./Header";
 
 export default function Layout({children}) {
--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -1,19 +1,16 @@
-export default function handler(context, res) {
-    fetch(process.env.FIRESTORE_URL)
-        .then(response => {response.json()
-            .then(data => {
-                const dataArray = data['documents']
-                const posts = dataArray.filter(post=>post.fields.publish && post.fields.publish.booleanValue === true)
-                    .map(post=>{
-                        const author = post.fields.author.stringValue
-                        const link = post.fields.link.stringValue
-                        const pictureSrc = post.fields.pictureSrc.stringValue
-                        const description = post.fields.description.stringValue
-                        const title = post.fields.title.stringValue
-                        const createTime = post.createTime
-                        return {author,link,pictureSrc,description,title,createTime}
-                    })
-                res.status(200).send({posts})
-            })
+export default async function handler(context, res) {
+    const response = await fetch(process.env.FIRESTORE_URL)
+    const data = await response.json()
+    const dataArray = data['documents']
+    const posts = dataArray.filter(post => post.fields.publish && post.fields.publish.booleanValue === true)
+        .map(post => {
+            const author = post.fields.author.stringValue
+            const link = post.fields.link.stringValue
+            const pictureSrc = post.fields.pictureSrc.stringValue
+            const description = post.fields.description.stringValue
+            const title = post.fields.title.stringValue
+            const createTime = post.createTime
+            return {author, link, pictureSrc, description, title, createTime}
         })
+    res.status(200).send({posts})
 }
